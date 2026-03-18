7d5d168599b3632fa2601880e6d8ae5d25dc491b
lucaa21ss-gif <lucassousaa21ss@gmail.com>
2026-03-18 18:59:52 -0300

    refactor: extract storage routes module

--- server/src/http/app-route-registrars.js
+++ server/src/http/app-route-registrars.js
@@ -2,7 +2,7 @@ import { registerUserRoutes } from "../modules/users/register-users-routes.js";
 import { registerIncidentRoutes } from "../modules/incident/register-incident-routes.js";
 import { registerApprovalRoutes } from "../modules/approvals/register-approval-routes.js";
 import { registerResilienceRoutes } from "../modules/resilience/register-resilience-routes.js";
-import { registerStorageRoutes } from "../modules/governance/register-storage-routes.js";
+import { registerStorageRoutes } from "../modules/storage/register-storage-routes.js";
 import { registerConfigRoutes } from "../modules/config-governance/register-config-routes.js";
 import { registerAuditRoutes } from "../modules/audit/register-audit-routes.js";
 import { registerObservabilityRoutes } from "../modules/observability/register-observability-routes.js";
