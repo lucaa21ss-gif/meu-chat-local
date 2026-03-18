12e99895170a9dd4c0ec6c076f9c3a86d7ef93e3
lucaa21ss-gif <lucassousaa21ss@gmail.com>
2026-03-18 13:12:06 -0300

    refactor: re-export createApp from factory

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-import { createConfiguredApp } from "./src/http/app-factory.js";
 import { runAsMainModule } from "./src/http/app-main-module.js";
 import { startConfiguredServer } from "./src/http/app-startup.js";
 import { createIntegrityRuntimeService } from "./src/modules/governance/integrity-service.js";
 
 export { createIntegrityRuntimeService };
-export const createApp = createConfiguredApp;
+export { createConfiguredApp as createApp } from "./src/http/app-factory.js";
 
 export async function startServer(port = 3001) {
   return startConfiguredServer({ port });
