2f199f38dd4164ba5c3b89b4888980072e4717ba
lucaa21ss-gif <lucassousaa21ss@gmail.com>
2026-03-19 12:14:43 -0300

    refactor(server): extrair wiring de create app

--- server/src/http/app-create.js
+++ server/src/http/app-create.js
@@ -3,6 +3,11 @@ import { client } from "../infra/ollama/ollama-client.js";
 import logger, { createHttpLogger } from "../infra/logging/logger.js";
 import { asyncHandler } from "./async-handler.js";
 import { resolveServerDir } from "./app-paths.js";
+import {
+  createAppBootstrapDeps,
+  createAppContextDeps,
+  createAppLocalsDeps,
+} from "./app-create-wiring.js";
 import { createStore } from "./app-store.js";
 import { createAppContext } from "./app-context.js";
 import { attachAppLocals, configureAppBootstrap } from "./app-bootstrap.js";
@@ -14,33 +19,28 @@ export function createConfiguredApp(deps = {}) {
 
   const app = express();
   const serverDir = resolveServerDir(import.meta.url, deps.serverDir);
-  const appContext = createAppContext({
-    deps,
-    store,
-    serverDir,
-    chatClient,
-    logger,
-    asyncHandler,
-  });
+  const appContext = createAppContext(
+    createAppContextDeps({
+      deps,
+      store,
+      serverDir,
+      chatClient,
+      logger,
+      asyncHandler,
+    }),
+  );
 
-  configureAppBootstrap(app, {
-    corsOrigin: appContext.corsOrigin,
-    webDir: appContext.webDir,
-    roleLimiter: appContext.roleLimiter,
-    createHttpLogger,
-    logger,
-    createTelemetryMiddleware: appContext.createTelemetryMiddleware,
-    express,
-  });
+  configureAppBootstrap(
+    app,
+    createAppBootstrapDeps({
+      appContext,
+      createHttpLogger,
+      logger,
+      express,
+    }),
+  );
 
-  attachAppLocals(app, {
-    backupService: appContext.backupService,
-    storageService: appContext.storageService,
-    capacityService: appContext.capacityService,
-    queueService: appContext.queueService,
-    baselineService: appContext.baselineService,
-    approvalService: appContext.approvalService,
-  });
+  attachAppLocals(app, createAppLocalsDeps(appContext));
 
   registerAppRoutes(app, appContext.routeDeps);
 
